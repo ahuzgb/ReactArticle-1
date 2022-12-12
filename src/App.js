f4993a3945774b907c618a0720ab3d52398b102d
valentina vittoria <valeviit@icloud.com>
2022-12-12 16:09:59 +0100

    vale commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header";
-
+import ApiArticle from "./Components/Header/Api-article";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Backend</h1>
+      <ApiArticle />
     </div>
   );
 }
