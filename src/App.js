df6156f363a16808c5fb9066b53f49a0786bc29b
valentina vittoria <valeviit@icloud.com>
2022-12-12 14:10:35 +0100

    edit css and create a api-article component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
 
+
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Backend</h1>
-      <button type="button" className="btn btn-primary">
-        Send
-      </button>
     </div>
   );
 }
---
+++ src/Components/Header/Api-article.js
@@ -0,0 +1,14 @@
+import "./api-article.css"
+
+
+import React from 'react';
+
+const ApiArticle = () => {
+    return (
+        <div>
+            <h1>Ciao Sono api</h1>
+        </div>
+    );
+}
+
+export default ApiArticle;
