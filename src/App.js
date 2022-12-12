de90a9fc18b4c668f8de77fd79872bcb6d406ad7
Tiefengnom <111353598+Tiefengnom@users.noreply.github.com>
2022-12-12 15:03:41 +0100

    fixed text and examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header";
+import JSONart from "./Components/Header/JSON";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Backend</h1>
+      <JSONart />
       <button type="button" className="btn btn-primary">
         Send
       </button>
